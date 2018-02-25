07f0fdc17b81b717a4220715a71d51bdacfd05c0
Joseph Ramos <jardix22@gmail.com>
2018-02-25 18:27:53 -0500

    Updated deply script to sync files.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,3 +4,14 @@ set -e
 
 composer global require "hirak/prestissimo:^0.3"
 composer install --prefer-dist
+
+if [[ $ENV == 'production' ]]; then
+  rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
+    --exclude-from "${PWD}/scripts/excludes.txt" \
+    --exclude 'settings.local.php' \
+    --delete --progress \
+    . ${USER}@${SERVER}:~/${DIRECTORY}/
+
+  # ssh -o StrictHostKeyChecking=no ${USER}@${SERVER} \
+  #   'cd ./${DIRECTORY}; ENV=prod /bin/sh -c "scripts/update.sh"'
+fi
